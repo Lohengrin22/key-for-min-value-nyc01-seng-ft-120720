6ed2fb96e70a6e51ee5a9b1307995f3da3456016
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 03:35:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   blank_value = name_hash
 name_hash.each do |key, value|
   if value < blank_value[key]
-  puts key[0]     
+  puts key[0]   
+  binding.pry 
   end 
   end 
 end
