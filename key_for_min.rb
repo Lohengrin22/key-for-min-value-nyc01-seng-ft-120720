b2a0e9a7c9e5fea3735a8f52c467995b41786526
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 03:29:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 require 'pry'
 def key_for_min_value(name_hash)
   name_hash = {:key1=>value1, :key2=> value2, :key3=> value3}
-  value = value1
+  value = name_hash[value1]
 name_hash.each do |key, value|
   if value < value1 && value2 && value3
   puts key     
