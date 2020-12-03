06c448d22e13d0ea3fdbb025e396ef74f9105249
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 03:46:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = ""
 name_hash.each do |key, value|
   if value < name_hash[key]
-    min_value = key 
+    min_value = "#{key}" 
   end 
   end 
   min_value 
