30b8cb36de91f03f25ccee2d7493caab02422fc8
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 03:19:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   min_value = 2 
   key =""
-name_hash.each_with_index do |key, value|
+name_hash.each do |key, value|
   binding.pry
   if value = 500
     puts key
