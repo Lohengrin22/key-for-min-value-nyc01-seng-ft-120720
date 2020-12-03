48970bc7afdd26decb8c005abc0dbd523377a5c2
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 03:26:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  name_hash
+  name_hash = {key1: value1, key2: value2, key3: value3}
   binding.pry
 name_hash.each do |key, value|
   name_hash[key] = value
