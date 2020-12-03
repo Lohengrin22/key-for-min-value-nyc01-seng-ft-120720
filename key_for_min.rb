13efc90c5e882ff8f96da0530187b9cc14584f1b
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 03:02:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
-
+  min_value = 2 
+  key =""
+name_hash.each do |i, k|
+  if k < 2
+    puts  
+  end 
+  end 
 end
